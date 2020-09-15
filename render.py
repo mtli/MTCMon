e0a9c0898c4d0179ad84828ea69fce7143287c0b
mt <martinli.work@gmail.com>
2020-09-15 17:23:38 -0400

    Adopt fixed-rate rendering approach and changed paths

---
+++ render.py
@@ -0,0 +1,32 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import sys, os, io
+# io is for python2 with UTF-8 support
+from time import sleep
+
+from flask import Flask
+from flask import render_template
+
+app = Flask('Dummy Flask')
+
+node_list = open('node-list.txt').readlines()
+node_list = [x.strip() for x in node_list] 
+
+def main():
+    sections = [None]*len(node_list)
+    with app.app_context():
+        while 1:
+            for i, node in enumerate(node_list):
+                try:
+                    sections[i] = open('/project_data/mtcmon/nodestats/' + node + '.html').read()
+                except Exception as e:
+                    print(e)
+                    sections[i] = render_template('section-error.html', machine_name=node)
+            html = render_template('index.html', sections=sections)
+            io.open('/var/www/mtcmon/index.html', 'w', encoding='utf8').write(html)
+            sleep(30)
+
+if __name__ == "__main__":
+    main()
--- run_monitor.sh
+++ run_monitor.sh
@@ -6,5 +6,5 @@ THIS_FILE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" > /dev/null && pwd )"
 
 # select input device 10 to prevent conflicting with ssh
 while read -u10 NODE_NAME; do
-	ssh $NODE_NAME bash "$THIS_FILE_DIR/run_single_node.sh" $NODE_NAME "$THIS_FILE_DIR/nodestats/"
+	ssh $NODE_NAME bash "$THIS_FILE_DIR/run_single_node.sh" $NODE_NAME "/project_data/mtcmon/nodestats/"
 done 10< "$THIS_FILE_DIR/node-list.txt"
