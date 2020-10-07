41644df8ec45de2de1433efb81b7308bdc26ef1e
vos <dvosler@cs.cmu.edu>
2020-10-06 20:04:49 -0400

    allows script to be run from anywhere by employing heavy use of variables

--- render.py
+++ render.py
@@ -1,35 +1,40 @@
-from __future__ import absolute_import
+from __future__ import absolute_import                                                                                 
 from __future__ import division
 from __future__ import print_function
 
 import sys, os, io
 # io is for python2 with UTF-8 support
 from time import sleep
-
 from flask import Flask
 from flask import render_template
 
 app = Flask('Dummy Flask')
+abs_path = os.path.dirname(__file__)
+web_path = os.path.join(abs_path, 'share/web/')
+nodestats = ('/project_data/mtcmon/nodestats/')
+web_index = web_path + '/index.html'
+node_list = os.path.join(abs_path, 'node-list.txt')
+node_list = open(node_list).readlines()
+node_list = [x.strip() for x in node_list]
 
-node_list = open('node-list.txt').readlines()
-node_list = [x.strip() for x in node_list] 
+if not os.path.isdir(web_path):
+      os.makedirs(web_path)
 
-if not os.path.isdir('/opt/MTCMon/share/web/'):
-    os.makedirs('/opt/MTCMon/share/web/')
-    
 def main():
     sections = [None]*len(node_list)
     with app.app_context():
         while 1:
             for i, node in enumerate(node_list):
                 try:
-                    sections[i] = open('/project_data/mtcmon/nodestats/' + node + '.html').read()
+                    sections[i] = open(nodestats + node + '.html').read()
                 except Exception as e:
                     print(e)
                     sections[i] = render_template('section-error.html', machine_name=node)
             html = render_template('index.html', sections=sections)
-            io.open('/opt/MTCMon/share/web/index.html', 'w', encoding='utf8').write(html)
+            io.open(web_index, 'w', encoding='utf8').write(html)
             sleep(30)
 
 if __name__ == "__main__":
     main()
+
+
