2cabed37f494c13b263dbac5ca6d07fd2ea122d2
vos <dvosler@cs.cmu.edu>
2020-10-07 09:05:32 -0400

    absolute path variable set

--- design2template.py
+++ design2template.py
@@ -6,18 +6,20 @@ import os
 import re
 import io # for python2.7
 
-in_file = 'index-design.html'
-out_dir = 'templates'
+abs_path = os.path.dirname(__file__)
+in_file = abs_path + '/index-design.html'                                                                              
+out_dir = abs_path + '/templates'
 out_file = 'index.html'
 
 if not os.path.isdir(out_dir):
-	os.makedirs(out_dir)
-	
+  os.makedirs(out_dir)
+  
 with io.open(in_file, encoding='utf8') as fin:
-	content = fin.read()
-	
+  content = fin.read()
+  
 sections = '{% for section in sections %}\n{{ section|safe }}\n{% endfor %}'
 content = re.sub('<!-- Content Start -->.*?<!-- Content End -->', sections, content, flags = re.DOTALL)
 
 with io.open(os.path.join(out_dir, out_file), 'w', encoding='utf8') as fout:
     fout.write(content)
+
