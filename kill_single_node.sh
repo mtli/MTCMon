f71d6e565de1ed4abd002421eaa7cec49bb3f1d0
mt <martinli.work@gmail.com>
2020-12-17 04:08:39 -0500

    Fix kill script bug

--- kill_single_node.sh
+++ kill_single_node.sh
@@ -3,7 +3,7 @@
 SCRIPT_NAME=$1
 MACHINE_NAME=$2
 
-prev_proc=`pgrep -u $(whoami) -lf python | grep $SCRIPT_NAME | cut -d' ' -f 1`
+prev_proc=`pgrep -u $(whoami) -af python | grep $SCRIPT_NAME | cut -d' ' -f 1`
 if [ -n "$prev_proc" ]; then
   echo "Killing previous procs on $MACHINE_NAME: $prev_proc"
   kill -9 $prev_proc
