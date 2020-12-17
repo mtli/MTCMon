497eb2fcbf0a341454c5e6c88599acc527f2de5e
mt <martinli.work@gmail.com>
2020-12-17 04:33:32 -0500

    Fix kill prompt

--- kill_single_node.sh
+++ kill_single_node.sh
@@ -5,6 +5,7 @@ MACHINE_NAME=$2
 
 prev_proc=`pgrep -u $(whoami) -af python | grep $SCRIPT_NAME | cut -d' ' -f 1`
 if [ -n "$prev_proc" ]; then
-  echo "Killing previous procs on $MACHINE_NAME: $prev_proc"
+  echo Killing previous procs on $MACHINE_NAME:
+  echo $prev_proc
   kill -9 $prev_proc
 fi
