fec7bd8ec9d4f55f437312531a319a20b67f7530
mt <martinli.work@gmail.com>
2021-10-22 01:16:18 -0400

    Bump ver to 0.3.0 and add node index section

--- design2template.py
+++ design2template.py
@@ -1,14 +1,16 @@
 from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
 
-import os
-import re
+import os, re
 import io # for python2.7
+
 
 in_file = 'index-design.html'
 out_dir = 'templates'
 out_file = 'index.html'
+
+use_toc = True
+node_list = 'node-list.txt'
+
 
 if not os.path.isdir(out_dir):
 	os.makedirs(out_dir)
@@ -16,6 +18,20 @@ if not os.path.isdir(out_dir):
 with io.open(in_file, encoding='utf8') as fin:
 	content = fin.read()
 	
+if use_toc:
+	node_list = io.open('node-list.txt', encoding='utf8').readlines()
+	node_list = [x.strip() for x in node_list] 
+
+	node_list_html = '	<ul>\n'
+	for node in node_list:
+		node_list_html += \
+			'		<li><a class="scroll-to" href="#%s">%s</a></li>\n' % \
+				(node, node)
+	node_list_html += '	</ul>\n'
+	content = re.sub('<!-- Index List Start -->.*?<!-- Index List End -->', node_list_html, content, flags = re.DOTALL)
+else:
+	content = re.sub('<!-- ToC Start -->.*?<!-- ToC End -->', '', content, flags = re.DOTALL)
+
 sections = '{% for section in sections %}\n{{ section|safe }}\n{% endfor %}'
 content = re.sub('<!-- Content Start -->.*?<!-- Content End -->', sections, content, flags = re.DOTALL)
 
--- static/usagebar.js
+++ static/usagebar.js
@@ -1,17 +1,17 @@
 "use strict";
 
 function usageBar() {
-    var color2 = document.body.dataset.ubColor2;
-    var color3 = document.body.dataset.ubColor3;
+	var color2 = document.body.dataset.ubColor2;
+	var color3 = document.body.dataset.ubColor3;
 
 	var elems = document.querySelectorAll(".usagebar > span");
 	for (var i = 0; i < elems.length; i++)
 	{
-        var w = parseFloat(elems[i].style.width);
-        if (w > 90)
-            elems[i].style.backgroundColor = color3;
-        else if(w > 60)
-            elems[i].style.backgroundColor = color2;
+		var w = parseFloat(elems[i].style.width);
+		if (w > 90)
+			elems[i].style.backgroundColor = color3;
+		else if(w > 60)
+			elems[i].style.backgroundColor = color2;
 	}
 }
 
