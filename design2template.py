624fb04f5376419fd39285b4b1f84136aecc5f38
mt <martinli.work@gmail.com>
2021-10-28 22:57:16 -0400

    Cherry pick from master: Bump ver to 0.3.0 and add node index section
    # Conflicts:
    #	design2template.py
    #	index-design.html (manually merged)
    #	static/style.css
    #	templates/section.html

--- design2template.py
+++ design2template.py
@@ -1,25 +1,41 @@
 from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
 
-import os
 import re
+from os import makedirs
+from os.path import join, dirname, isdir
 import io # for python2.7
 
-abs_path = os.path.dirname(__file__)
-in_file = abs_path + '/index-design.html'                                                                              
-out_dir = abs_path + '/templates'
+abs_path = dirname(__file__)
+in_file = join(abs_path, 'index-design.html')                                                                             
+out_dir = join(abs_path, 'templates')
 out_file = 'index.html'
 
-if not os.path.isdir(out_dir):
-  os.makedirs(out_dir)
-  
+use_toc = True
+node_list = 'node-list.txt'
+
+
+if not isdir(out_dir):
+	makedirs(out_dir)
+
 with io.open(in_file, encoding='utf8') as fin:
-  content = fin.read()
-  
+	content = fin.read()
+
+if use_toc:
+	node_list = io.open('node-list.txt', encoding='utf8').readlines()
+	node_list = [x.strip() for x in node_list] 
+
+	node_list_html = '	<ul>\n'
+	for node in node_list:
+		node_list_html += \
+			'		<li><a class="scroll-to" href="#%s">%s</a></li>\n' % \
+				(node, node)
+	node_list_html += '	</ul>\n'
+	content = re.sub('<!-- Index List Start -->.*?<!-- Index List End -->', node_list_html, content, flags = re.DOTALL)
+else:
+	content = re.sub('<!-- ToC Start -->.*?<!-- ToC End -->', '', content, flags = re.DOTALL)
+
 sections = '{% for section in sections %}\n{{ section|safe }}\n{% endfor %}'
 content = re.sub('<!-- Content Start -->.*?<!-- Content End -->', sections, content, flags = re.DOTALL)
 
-with io.open(os.path.join(out_dir, out_file), 'w', encoding='utf8') as fout:
+with io.open(join(out_dir, out_file), 'w', encoding='utf8') as fout:
     fout.write(content)
-
--- humanize_time.py
+++ humanize_time.py
@@ -1,53 +1,56 @@
 # modified from http://stackoverflow.com/a/26781642/1492614
 
+from __future__ import absolute_import
+from __future__ import division
+
 def humanize_time(amount, units = 'seconds'):    
 
-    def process_time(amount, units):
+	def process_time(amount, units):
 
-        INTERVALS = [   1, 60, 
-                        60*60, 
-                        60*60*24, 
-                        60*60*24*7, 
-                        60*60*24*7*4, 
-                        60*60*24*7*4*12, 
-                        60*60*24*7*4*12*100,
-                        60*60*24*7*4*12*100*10]
-        NAMES = [('second', 'seconds'),
-                 ('minute', 'minutes'),
-                 ('hour', 'hours'),
-                 ('day', 'days'),
-                 ('week', 'weeks'),
-                 ('month', 'months'),
-                 ('year', 'years'),
-                 ('century', 'centuries'),
-                 ('millennium', 'millennia')]
+		INTERVALS = [   1, 60, 
+						60*60, 
+						60*60*24, 
+						60*60*24*7, 
+						60*60*24*7*4, 
+						60*60*24*7*4*12, 
+						60*60*24*7*4*12*100,
+						60*60*24*7*4*12*100*10]
+		NAMES = [('second', 'seconds'),
+				 ('minute', 'minutes'),
+				 ('hour', 'hours'),
+				 ('day', 'days'),
+				 ('week', 'weeks'),
+				 ('month', 'months'),
+				 ('year', 'years'),
+				 ('century', 'centuries'),
+				 ('millennium', 'millennia')]
 
-        result = []
+		result = []
 
-        unit = list(map(lambda a: a[1], NAMES)).index(units)
-        # Convert to seconds
-        amount = amount * INTERVALS[unit]
+		unit = list(map(lambda a: a[1], NAMES)).index(units)
+		# Convert to seconds
+		amount = amount * INTERVALS[unit]
 
-        for i in range(len(NAMES)-1, -1, -1):
-            a = amount // INTERVALS[i]
-            if a > 0: 
-                result.append( (a, NAMES[i][1 % a]) )
-                amount -= a * INTERVALS[i]
+		for i in range(len(NAMES)-1, -1, -1):
+			a = amount // INTERVALS[i]
+			if a > 0: 
+				result.append( (a, NAMES[i][1 % a]) )
+				amount -= a * INTERVALS[i]
 
-        return result
+		return result
 
-    rd = process_time(int(amount), units)
-    cont = 0
-    for u in rd:
-        if u[0] > 0:
-            cont += 1
+	rd = process_time(int(amount), units)
+	cont = 0
+	for u in rd:
+		if u[0] > 0:
+			cont += 1
 
-    buf = ''
-    i = 0
-    for u in rd:
-        if u[0] > 0:
-            buf += "%d%s" % (u[0], u[1][0])
-            cont -= 1
-        i += 1
+	buf = ''
+	i = 0
+	for u in rd:
+		if u[0] > 0:
+			buf += "%d%s" % (u[0], u[1][0])
+			cont -= 1
+		i += 1
 
-    return buf
+	return buf
